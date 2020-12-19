85755a04c8017b80f2b10e5fdf0bedba1f12e88d
Dustin <7419947+whaaaley@users.noreply.github.com>
2020-12-18 19:44:56 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const readline = require('readline')
+const rl = require('readline').createInterface({ input: process.stdin })
 const SourceMap = require('@parcel/source-map').default
 const args = require('./parse-argv')(process.argv)
+
+let stdin = ''
+rl.on('line', data => stdin += data)
+rl.close()
 
 const map = args['--map']
 const out = args['--out']
 
-let stdin = ''
-const rl = readline.createInterface({ input: process.stdin })
-rl.on('line', data => stdin += data)
-
 const paths = Array.isArray(map) ? map : [map]
-const files = stdin === '' ? [] : [JSON.parse(stdin)]
 const sourceMap = new SourceMap()
+
+if (stdin !== '') {
+  const data = JSON.parse(stdin)
+  sourceMap.addRawMappings(data)
+}
 
 for (let i = 0; i < paths.length; i++) {
   const file = fs.readFileSync(paths[i])
   const data = JSON.parse(file)
-  files.push(data)
+  sourceMap.addRawMappings(data)
 }
 
-if (files.length < 1) {
-  console.log('Too few source maps. Exiting...')
-  process.exit()
-}
-
-for (let i = 0; i < files.length; i++) {
-  sourceMap.addRawMappings(files[i])
-}
-
-const go = async () => {
+const output = async () => {
   const data = await sourceMap.stringify()
 
   if (typeof out === 'string') {
-    console.log('Writing to', out)
     fs.writeFileSync(out, data)
   } else {
-    // No --out flag. Writing to stdout...
     process.stdout.write(data)
   }
 }
 
-go()
+output()
